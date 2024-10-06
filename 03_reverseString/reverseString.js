ba2ed7edb60011c3a6cd6a661cf140e5481222aa
AO <103045021+AOPrukcu@users.noreply.github.com>
2024-10-06 21:16:06 +0300

    finished 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let reverseInput = "";
+    for (let index = input.length -1 ; index >= 0; index--) {
+        reverseInput += input.charAt(index)
+         
+        
+    }
+    return reverseInput;
 };
-
-// Do not edit below this line
+ // Do not edit below this line
 module.exports = reverseString;
