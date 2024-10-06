19b64944ef05f676cce786ff83564cffdb8f7de6
AO <103045021+AOPrukcu@users.noreply.github.com>
2024-10-06 21:06:37 +0300

    finished 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (word, repeatNumber) {
+  if (repeatNumber < 0) {
+    return "ERROR";
+  }
+  let repeatedWord = "";
+  for (i = 0; i < repeatNumber; i++) {
+    repeatedWord += word;
+    console.log(repeatedWord);
+  }
+  return repeatedWord;
 };
+
+repeatString("hey", 4);
 
 // Do not edit below this line
 module.exports = repeatString;
