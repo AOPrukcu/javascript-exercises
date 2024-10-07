261c2ee2534d962ecb73f0dcd120029027b72716
AO <103045021+AOPrukcu@users.noreply.github.com>
2024-10-07 22:02:42 +0300

    finished 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius = (fahrenheit - 32) * 5/9;
+  return +celcius.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = celcius*9/5 + 32;
+  return +fahrenheit.toFixed(1)
+
 };
+
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
