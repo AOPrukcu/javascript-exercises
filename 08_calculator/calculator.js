552fe0d47b91d9ba5f0aa2382aa7b39509acfe0e
AO <103045021+AOPrukcu@users.noreply.github.com>
2024-10-07 21:50:27 +0300

    finished calculator exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,60 @@
-const add = function() {
+const add = function(...arg) {
+	let sum = 0;
+  arg.forEach(element => {
+    sum +=element
+    
+  })
+  return sum ;;
+};
+
+const subtract = function(...arg) {
+  let result = 0;
+  arg.forEach(element => {
+    result -=element
+    
+  })
+  result += arg[0]*2
+ 
+  return result ;;
 	
 };
 
-const subtract = function() {
+const sum = function(array) {
+  let sum = 0;
+  array.forEach(element => {
+    sum +=element
+    
+  })
+  return sum ;;
 	
 };
 
-const sum = function() {
+const multiply = function(array) {
+  let sum = 1;
+  array.forEach(element => {
+    sum *=element
+    
+  })
+  return sum ;;
+
+};
+
+const power = function(num1,num2) {
+  return num1**num2
 	
 };
 
-const multiply = function() {
+const factorial = function(num) {
+  let element = 1;
+  if (num ==0) {
+    return 1 ;
+  }
+  for (let index = 1; index <= num; index++) {
+     element *= index;
+    
+  }
+  return element
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
