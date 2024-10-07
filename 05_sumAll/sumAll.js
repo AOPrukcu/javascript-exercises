f1d7a6586a673377f1622cd38b343963eac13fa6
AO <103045021+AOPrukcu@users.noreply.github.com>
2024-10-07 20:44:16 +0300

    finished 05 with all the tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    if (num1 < 0 || num2 < 0 || !( Number.isInteger(num1)) || !(Number.isInteger(num2) )) {
+        return "ERROR"
+    } else{
+        if (num1>num2) {
+            return num1*(num1+1)/2 -num2*(num2+1)/2 +num2
+        } else {
+            return num2*(num2+1)/2 -num1*(num1+1)/2 + num1
+        }
+         
+    }
 
 };
 
