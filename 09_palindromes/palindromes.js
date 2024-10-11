faedcb006d24b8ed76f7ea26646ae9c2da7e0acd
AO <103045021+AOPrukcu@users.noreply.github.com>
2024-10-11 17:59:49 +0300

    finished 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let arr = word.split("")
+    let result = arr.filter((element) => (element != "!")&&(element != ",") && (element!=" ") && (element != ".")   )
+    let temp = result.slice()
+    let reverse = result.reverse()
+    palindrome = reverse.join('').toLowerCase() === temp.join('').toLowerCase()
+    return palindrome
+
+     
+    
 
 };
+str = "adfasfasafasf"
+str1 ="abcba"
+palindromes(str1)
+
 
 // Do not edit below this line
 module.exports = palindromes;
