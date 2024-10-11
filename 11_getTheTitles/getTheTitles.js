3e0bbf141c5eb231b3cd3b7fc52e8f93e3964590
AO <103045021+AOPrukcu@users.noreply.github.com>
2024-10-11 18:54:33 +0300

    finished 11_getthetitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+
+    let titles = arr.map((element) => element.title)
+    return titles
 
 };
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+  
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
